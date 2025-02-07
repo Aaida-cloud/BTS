b8c2babe8e286dcc5d663b55c5d2e5a08b209701
Aaida-cloud <aaidazafar5@gmail.com>
2025-02-07 12:11:01 +0500

    add authorization

--- app/policies/project_policy.rb
+++ app/policies/project_policy.rb
@@ -1,4 +1,13 @@
 class ProjectPolicy < ApplicationPolicy
+
+  def index?
+    user.manager? || user.qa? || user.developer?
+  end
+
+  def show?
+    user.manager? || user.qa? || (user.developer? && record.users.include?(user))
+  end
+
   def create?
     user.manager?
   end
@@ -10,16 +19,13 @@ class ProjectPolicy < ApplicationPolicy
   def destroy?
     user.manager?
   end
-  # NOTE: Up to Pundit v2.3.1, the inheritance was declared as
-  # `Scope < Scope` rather than `Scope < ApplicationPolicy::Scope`.
-  # In most cases the behavior will be identical, but if updating existing
-  # code, beware of possible changes to the ancestors:
-  # https://gist.github.com/Burgestrand/4b4bc22f31c8a95c425fc0e30d7ef1f5
+
+  def add_remove_users?
+    user.manager?
+  end
+
 
   class Scope < ApplicationPolicy::Scope
-    # NOTE: Be explicit about which records you allow access to!
-    # def resolve
-    #   scope.all
-    # end
+    
   end
 end
